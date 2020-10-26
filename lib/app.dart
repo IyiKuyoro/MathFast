07d263d07f1d3d15bcbea72fe135ec8808666b97
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2020-10-26 13:52:35 -0400

    setup home page

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'Pages/home.dart';
 
-class MyApp extends StatelessWidget {
+class MathApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Math Quiz',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Math Quiz'),
+      title: 'Flutter Tutorial',
+      home: Home(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MathApp());
 }
