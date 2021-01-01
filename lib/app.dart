ad1b6d7e8323a9cb0a7f85b7aed38fd863a64f4d
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2020-12-31 19:35:28 -0500

    - add luncher image - setup env variables - remove files

--- lib/app.dart
+++ lib/app.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
-
-import 'Pages/home.dart';
-import 'store/Results.dart';
 
 class MathApp extends StatelessWidget {
-  final Store<List<Result>> store;
-
-  MathApp(this.store);
-
   @override
   Widget build(BuildContext context) {
-    return new StoreProvider<List<Result>>(
-      store: store,
-      child: new MaterialApp(
-        title: 'Flutter Tutorial',
-        home: new Home(),
+    return MaterialApp(
+      title: 'Flutter Tutorial',
+      home: Center(
+        child: Container(
+          child: Image.asset(
+            'assets/MathFastLogo.png',
+            width: 40,
+            height: 40,
+          ),
+        ),
       ),
     );
   }
